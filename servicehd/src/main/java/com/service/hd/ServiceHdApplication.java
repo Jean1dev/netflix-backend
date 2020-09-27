97c053ee935ddae0c66e0372aaa273cc348583db
Jean <jeanlucafp@gmail.com>
2020-09-27 20:24:54 -0300

    criado servico q faz o stream hd

---
+++ servicehd/src/main/java/com/service/hd/ServiceHdApplication.java
@@ -0,0 +1,13 @@
+package com.service.hd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHdApplication.class, args);
+	}
+
+}
